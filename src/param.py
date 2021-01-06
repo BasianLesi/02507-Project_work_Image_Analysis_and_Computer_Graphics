2ac3bc5e95b25c8aca4d91d69fcd8ab9eb65bafe
BasianLesi <lroni10094@gmail.com>
2021-01-06 23:26:11 +0100

    Merging 3 pcd

---
+++ src/param.py
@@ -0,0 +1,8 @@
+# source_path = "../data/plyfolder/points19.ply"
+# target_path = "../data/plyfolder/points20.ply"
+
+source_path = "../data/plyfolder/points0.ply"
+target_path = "0601-stitch_19-20.pcd"
+
+voxel_size = 0.2
+threshold = 0.2
